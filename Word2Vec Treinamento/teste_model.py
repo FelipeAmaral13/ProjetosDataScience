e311aeb476f464942766d776d53fbe1fb5054508
FelipeAmaral13 <felipengmec@gmail.com>
2021-10-26 22:28:44 -0300

    Teste modelo Rl novo

--- Word2Vec Treinamento/teste_model.py
+++ Word2Vec Treinamento/teste_model.py
@@ -1,15 +1,18 @@
 import os
 import pickle
-from model_cbow import *
+from model_cbow import tokenizador, combinacao_de_vetores_por_soma
+from gensim.models import KeyedVectors
 
 with open(os.path.join(os.getcwd(), 'rl_cbow.pkl'), "rb") as f:
-    classificador = pickle.load(f)
+    model = pickle.load(f)
+    
+w2v_modelo_cbow = KeyedVectors.load_word2vec_format(os.path.join(os.getcwd(), 'modelo_cbow.txt'))
 
 
-texto_teste = "Uninove cria técnica de classificação de OS"
+texto_teste = "Museu abre no feirado"
 
 teste_tokens = tokenizador(texto_teste)
 teste_vetor = combinacao_de_vetores_por_soma(teste_tokens, w2v_modelo_cbow)
-teste_categoria = classificador.predict(teste_vetor)
+teste_categoria = model.predict(texto_teste)
 
 categoria = teste_categoria[0].capitalize()
